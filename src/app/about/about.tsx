15ee9ba706f88dc1071ce6a73fed7354e92009ab
ArnaudBay <arnaudbayale@gmail.com>
2025-03-22 02:27:47 +0100

    About

--- src/app/about/about.tsx
+++ src/app/about/about.tsx
@@ -1,39 +1,30 @@
-
 //import Menu from '../menu';
 
 export default function About() {
   return (
     <div>
-      
       <main className="px-6 py-20">
         <div className="max-w-7xl mx-auto">
           <div className="grid md:grid-cols-2 gap-16 items-start">
-
-
-            
             <h1 className="text-6xl font-serif leading-tight">
               Who We Are
             </h1>
 
-           
             <div className="space-y-8 text-xl leading-relaxed">
-  
-            <p>
+              <p>
                 Je suis Arnaud Bayale, un développeur passionné originaire de la Centrafrique. 
-                Spécialise dans le développement web et mobile, j aime concevoir des solutions 
+                Spécialisé dans le développement web et mobile, j&apos;aime concevoir des solutions 
                 innovantes pour répondre aux besoins des utilisateurs. Mon objectif est de 
                 continuer à apprendre et à évoluer dans le domaine de la technologie.
-            </p>
+              </p>
 
-  
-                <p>
-                    Avec une formation en informatique de gestion, j ai travaillé sur divers projets, 
-                    allant de la gestion scolaire à la digitalisation des bibliothèques universitaires. 
-                    Je cherche constamment à améliorer mes compétences, notamment en développement 
-                    d'applications Android et en intégration de nouvelles technologies comme Flutter et Node.js.
-                </p>
-</div>
-
+              <p>
+                Avec une formation en informatique de gestion, j&apos;ai travaillé sur divers projets, 
+                allant de la gestion scolaire à la digitalisation des bibliothèques universitaires. 
+                Je cherche constamment à améliorer mes compétences, notamment en développement 
+                d&apos;applications Android et en intégration de nouvelles technologies comme Flutter et Node.js.
+              </p>
+            </div>
           </div>
         </div>
       </main>
@@ -41,9 +32,6 @@ export default function About() {
       {/* Footer */}
       <footer className="px-6 py-20 bg-gray-100">
         <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-12">
-
-
-         
           <div>
             <h3 className="text-2xl font-serif mb-8">Bergen</h3>
             <address className="not-italic">
@@ -58,7 +46,6 @@ export default function About() {
             </p>
           </div>
 
-          
           <div>
             <h3 className="text-2xl font-serif mb-8">About</h3>
             <nav className="flex flex-col space-y-4">
@@ -68,7 +55,6 @@ export default function About() {
             </nav>
           </div>
 
-         
           <div>
             <h3 className="text-2xl font-serif mb-8">Projects</h3>
             <nav className="flex flex-col space-y-4">
